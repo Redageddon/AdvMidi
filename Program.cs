2e62c74ce50fd5f0abb1b0294a3b9c52d87b52b3
Rubiksmaster02 <therealrubiksmaster@gmail.com>
2019-12-21 08:32:08 -0800

    First push

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Melanchall.DryWetMidi.Devices;
+
+namespace MidiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var inputDevices = InputDevice.GetAll().ToArray();
+            
+            #region Input
+            using var inputDevice = InputDevice.GetByName(inputDevices[0].ToString());
+            inputDevice.EventReceived += OnEventReceived;
+
+            do {
+                while (! Console.KeyAvailable) {
+                    inputDevice.StartEventsListening();
+                }       
+            } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+            // NoteOnEvent
+            
+            void OnEventReceived(object sender, MidiEventReceivedEventArgs e)
+            {
+                var midiDevice = (MidiDevice) sender;
+                string keyEvent = e.Event.ToString();
+                var inputs = keyEvent.Split(',');
+                bool on = Convert.ToBoolean(int.Parse(inputs[1]));
+                int note = int.Parse(inputs[0]);
+                var input = (on, note);
+                
+                Console.WriteLine($"{input.on} {input.note}");
+            }
+            #endregion
+            
+            
+        }
+    }
+}
