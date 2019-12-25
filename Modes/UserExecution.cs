fe86eeb842c3023668254e06a22590981dd5f668
Rubiksmaster02 <therealrubiksmaster@gmail.com>
2019-12-25 10:33:15 -0800

    shorter uuser execution

--- Modes/UserExecution.cs
+++ Modes/UserExecution.cs
@@ -16,8 +16,7 @@ namespace AdvMidi
         public static void Execution()
         {
             PreloadAllExecutionFiles();
-            (int inputTemp, int outputTemp) = FlowInitiation.Devices();
-            using var outputDevice = OutputDevice.GetById(outputTemp);
+            int inputTemp = FlowInitiation.Devices().Item1;
             using var inputDevice = InputDevice.GetById(inputTemp);
 
             inputDevice.EventReceived += OnEventReceived;
@@ -28,17 +27,11 @@ namespace AdvMidi
 
             void OnEventReceived(object sender, MidiEventReceivedEventArgs e)
             {
-                if (e.Event.EventType.ToString() == "NoteOn")
-                {
-                    string keyEvent = e.Event.ToString();
-                    var inputs = keyEvent.Split(',');
-                    int note = int.Parse(inputs[0]);
-                    if (int.Parse(inputs[1]) == 127)
-                    {
-                        DoExecution(note);
-                        Console.WriteLine(note);
-                    }
-                }
+                if (e.Event.EventType.ToString() != "NoteOn") return;
+                string keyEvent = e.Event.ToString();
+                var inputs = keyEvent.Split(',');
+                int note = int.Parse(inputs[0]);
+                if (int.Parse(inputs[1]) == 127) DoExecution(note);
             }
         }
         
@@ -67,18 +60,9 @@ namespace AdvMidi
 
         private static async void DoExecution(int note)
         {
-            Dictionary<int, int> notes = new Dictionary<int, int>()
-            {
-                {11,  0}, {12,  1}, {13,  2}, {14,  3}, {15,  4}, {16,  5}, {17,  6}, {18,  7}, {21,  8}, {22,  9}, 
-                {23, 10}, {24, 11}, {25, 12}, {26, 13}, {27, 14}, {28, 15}, {31, 16}, {32, 17}, {33, 18}, {34, 19}, 
-                {35, 20}, {36, 21}, {37, 22}, {38, 23}, {41, 24}, {42, 25}, {43, 26}, {44, 27}, {45, 28}, {46, 29}, 
-                {47, 30}, {48, 31}, {51, 32}, {52, 33}, {53, 34}, {54, 35}, {55, 36}, {56, 37}, {57, 38}, {58, 39}, 
-                {61, 40}, {62, 41}, {63, 42}, {64, 43}, {65, 44}, {66, 45}, {67, 46}, {68, 47}, {71, 48}, {72, 49},
-                {73, 50}, {74, 51}, {75, 52}, {76, 53}, {77, 54}, {78, 55}, {81, 56}, {82, 57}, {83, 58}, {84, 59}, 
-                {85, 60}, {86, 61}, {87, 62}, {88, 63}
-            };
-            await CaseT[notes[note]].RunAsync();
-            
+            int x = note - 11 - 2 * (note / 10 - 1);
+            Console.WriteLine(x);
+            await CaseT[x].RunAsync();
         }
     }
 }
