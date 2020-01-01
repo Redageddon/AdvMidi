0cf83219daea4f6e292cc7ed3315e5aa59501d75
Rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-01-01 10:43:56 -0800

    dont download me now

--- Modes/MainWindow.xaml.cs
+++ Modes/MainWindow.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 
